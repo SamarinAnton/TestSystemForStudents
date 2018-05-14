df9471bbda0a6eb6f03e3a042c5851fada9f2e84
Samarin Anton <noguto@mail.ru>
2018-05-15 02:40:08 +0300

    Added new parameters in application.properties

--- src/main/java/ru/testsForStudents/DAO/UserDAO.java
+++ src/main/java/ru/testsForStudents/DAO/UserDAO.java
@@ -1,10 +1,11 @@
 package ru.testsForStudents.DAO;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import ru.testsForStudents.entity.BaseEntity;
 import ru.testsForStudents.entity.User;
 
 import java.util.List;
 
-public interface UserDAO extends CrudRepository<User, Long>{
+public interface UserDAO extends JpaRepository<User, Long> {
 }
--- src/main/java/ru/testsForStudents/controller/Main.java
+++ src/main/java/ru/testsForStudents/controller/Main.java
@@ -1,4 +1,4 @@
-package ru.testsForStudents;
+package ru.testsForStudents.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
